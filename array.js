ddd2500d46a4c0ae6d6271534bfd261242cbeb15
mdaktarhossen <mdaktarhossen20@gmail.com>
2024-12-24 20:27:50 +0600

    a

--- array.js
+++
@@ -1,6 +0,0 @@
-<h2>Today i will describe array and object </h2>;
-<h3>Operation in array way</h3>;
-<ul>Create a new object in a arry</ul>;
-<ul>Delete a object from array and spacific property</ul>;
-<ul>Update a array object property</ul>;
-<ul>Traves a arry in differe way</ul>;
